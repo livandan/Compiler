93ced8f3a2f0097468839935ea00847edbdec53c
livandan <livandan@sjtu.edu.cn>
2026-06-19 19:01:24 +0800

    fix: remove 15s timeout in run.sh. All tests passed.

--- run.sh
+++ run.sh
@@ -27,9 +27,6 @@ for INDEX in {1..50}; do
   fi
 done
 
-echo "IR generating tests: ${IR_success_count} success out of 50"
-
-
 #echo "compiling debugging.ll into assembly..."
 #clang -S --target=riscv32-unknown-elf -O0 "RCompiler-Testcases/working_space/debugging.ll" -o "RCompiler-Testcases/working_space/debugging.s"
 #if [ $? -eq 0 ]; then
@@ -71,7 +68,7 @@ for IN_FILE in RCompiler-Testcases/working_space/t*.in; do
   fi
   ((total_count++))
 
-  timeout 15s reimu -f="$S_FILE" -i="$IN_FILE" -o="$MY_OUT_FILE" -s=200000000
+  reimu -f="$S_FILE" -i="$IN_FILE" -o="$MY_OUT_FILE" -s=200000000
   if [ $? -eq 0 ]; then
     diff "$MY_OUT_FILE" "$OUT_FILE" -Z
     if [ $? -eq 0 ]; then
@@ -85,8 +82,6 @@ for IN_FILE in RCompiler-Testcases/working_space/t*.in; do
   fi
 done
 
-echo ""
-echo "Small tests: ${success_count} success out of ${total_count}"
 
 echo ""
 echo ""
@@ -98,7 +93,7 @@ code_gen_success_count=0
 mem2reg_success_count=0
 
 for INDEX in {1..50}; do
-  timeout 15s reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
+  reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
   if [ $? -eq 0 ]; then
     diff "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.out" -Z
     if [ $? -eq 0 ]; then
@@ -109,7 +104,7 @@ for INDEX in {1..50}; do
       echo "********************************************************************************** ${INDEX} failed in diff"
       clang -S --target=riscv32-unknown-elf -O0 "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.ll" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s"
       if [ $? -eq 0 ]; then
-        timeout 15s reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
+        reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
         if [ $? -eq 0 ]; then
           diff "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.out" -Z
           if [ $? -eq 0 ]; then
@@ -129,7 +124,7 @@ for INDEX in {1..50}; do
     echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ${INDEX} failed in executing reimu"
     clang -S --target=riscv32-unknown-elf -O0 "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.ll" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s"
     if [ $? -eq 0 ]; then
-      timeout 15s reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
+      reimu -f="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_mem2reg.s" -i="RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.in" -o "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" -s=200000000
       if [ $? -eq 0 ]; then
         diff "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}_my_output.out" "RCompiler-Testcases/IR-1/src/comprehensive${INDEX}/comprehensive${INDEX}.out" -Z
         if [ $? -eq 0 ]; then
@@ -147,5 +142,7 @@ for INDEX in {1..50}; do
   fi
 done
 
+echo "IR generating tests: ${IR_success_count} success out of 50"
+echo "Small tests: ${success_count} success out of ${total_count}"
 echo "codegen tests: ${code_gen_success_count} success out of 50"
 echo "mem2reg tests: ${mem2reg_success_count} success out of 50"
